17e558c4c141b5e03347f0411435704cdfbf446a
Gustavo Jardim <gustavo.spadotto@gmail.com>
2021-07-20 17:29:18 -0300

    Created BotaoSalvar and BotaoCancelar components

--- src/@material/Button.js
+++ src/@material/Button.js
@@ -1,4 +1,6 @@
+import Button from '@material-ui/core/Button';
 import {makeStyles} from '@material-ui/core';
+import {withStyles} from '@material-ui/core/styles';
 
 export const fabStyles = makeStyles(() => ({
     fab: {
@@ -14,3 +16,36 @@ export const fabStyles = makeStyles(() => ({
         marginLeft: '10px',
     }
 }));
+
+export const BotaoSalvar = withStyles ({
+    root: {
+        backgroundColor: 'white',
+        borderRadius: '20px',
+        border: '1px solid green',
+        color: 'green',
+        '&:hover': {
+            backgroundColor: 'lightgray',
+            borderColor: 'none',
+            boxShadow: 'none',
+        },
+        '&:disabled': {
+            border: '1px solid lightgray'
+        },
+    },
+})(Button);
+
+export const BotaoCancelar = withStyles ({
+    root: {
+        backgroundColor: 'transparent',
+        boxShadow: 'none',
+        borderRadius: '20px',
+        color: 'red',
+        '&:hover': {
+            backgroundColor: 'rgba(244, 67, 54, 0.05)',
+            border: 'none',
+            boxShadow: 'none',
+        },
+    },
+})(Button);
+
+// export 
