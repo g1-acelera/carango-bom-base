0048a3e78d7169d241e79856ba98b56cdad6cb1d
Bruno Alves <bruno-alves2010@live.com>
2021-07-21 19:09:35 -0300

    Update api.routes.js

--- src/shared/api/api.routes.js
+++ src/shared/api/api.routes.js
@@ -1,5 +1,5 @@
 const API = process.env.REACT_APP_ENV === "Development" ?
     "https://carango-bom-api.herokuapp.com" :
-    "https://carango-bom-api.herokuapp.com";
+    "https://localhost";
 
 export default API;
