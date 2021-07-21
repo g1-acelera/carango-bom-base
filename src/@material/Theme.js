0a0c447926d46dedbf30f10f8b0480693d86d1db
Bruno Alves <bruno-alves2010@live.com>
2021-07-21 18:29:08 -0300

    Update Theme.js

--- src/@material/Theme.js
+++ src/@material/Theme.js
@@ -1,6 +1,7 @@
 import pink from '@material-ui/core/colors/pink';
 import {ptBR} from '@material-ui/core/locale';
-import {createTheme, makeStyles} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core';
+import {createTheme} from "@material-ui/core/styles";
 import {red} from "@material-ui/core/colors";
 
 export const defaultStyles = makeStyles((theme) => ({
