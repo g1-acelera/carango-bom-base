4b12daebc16986f8dcdeb3d8c7d6dff6ac39cf2a
Gustavo Jardim <gustavo.spadotto@gmail.com>
2021-07-21 14:48:56 -0300

    Update Marca.js

--- src/pages/cadastro-marca/Marca.js
+++ src/pages/cadastro-marca/Marca.js
@@ -1,9 +1,19 @@
-const Marca = {
-    modelo() {
+import validaTamanhoMinimo from '../../shared/validators/tamanho-minimo/tamanho-minimo'
+import validaObrigatoriedade from '../../shared/validators/obrigatorio/obrigatorio'
+
+class Marca {
+    static ehValido = false
+
+    static modelo() {
         return {
             nome: '',
         }
-    },
-};
+    }
+    static validacoesNome(valorDoCampo) {
+        const validacao = validaTamanhoMinimo(valorDoCampo, 3) == ""|| validaObrigatoriedade(valorDoCampo) == ""
+        this.ehValido = !validacao
+        return validacao
+    }
+}
 
 export default Marca;
