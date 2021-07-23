f3e083fb6d502b6fb2da158555f69d4ce0a38e87
Bruno Alves <bruno-alves2010@live.com>
2021-07-23 11:17:16 -0300

    Add route constants for pages

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import React, {lazy, Suspense} from "react";
 import {Route, Switch} from "react-router-dom";
+import ROTAS from "../shared/constants/rotas.const";
 
 const CadastroMarca = lazy(() => import("../pages/cadastro-marca/CadastroMarca"));
 const ListagemMarcas = lazy(() => import("../pages/listagem-marca/ListagemMarcas"));
@@ -8,9 +9,9 @@ export default function RoutesContainer() {
     return (
         <Suspense fallback={<></>}>
             <Switch>
-                <Route path="/cadastro-marca" component={CadastroMarca}/>
-                <Route path='/alteracao-marca/:id' component={CadastroMarca}/>
-                <Route path="/" component={ListagemMarcas}/>
+                <Route path={ROTAS.CADASTRO_MARCA} component={CadastroMarca}/>
+                <Route path={ROTAS.ALTERACAO_MARCA} component={CadastroMarca}/>
+                <Route path={ROTAS.MARCAS} component={ListagemMarcas}/>
             </Switch>
         </Suspense>
     );
---
+++ src/shared/constants/rotas.const.js
@@ -0,0 +1,7 @@
+const ROTAS = {
+    CADASTRO_MARCA: "/cadastro-marca",
+    ALTERACAO_MARCA: "/alteracao-marca/:id",
+    MARCAS: "/marcas"
+};
+
+export default ROTAS;
