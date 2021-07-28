3d77d44da4af6b2fdffbcc8f4c2f5d1ee56ebec5
Bruno Alves <bruno-alves2010@live.com>
2021-07-28 11:31:28 -0300

    Add attributes to MenuLateral component tests

--- src/shared/components/Menu/MenuSuperior.jsx
+++ src/shared/components/Menu/MenuSuperior.jsx
@@ -3,30 +3,34 @@ import {AppBar, IconButton, Toolbar} from "@material-ui/core";
 import {Menu} from "@material-ui/icons";
 import {BotaoEntrar} from "../../../@material/Button";
 import sidebarStyles from "../../../@material/Sidebar";
+import {useAutenticacaoContext} from "../../context/autenticacao.context";
 
 const MenuSuperior = ({menuClick}) => {
+    // const {dadosUsuario} = useAutenticacaoContext();
     const classes = sidebarStyles();
 
     return (
-        <AppBar position="fixed" className={`${classes.appBar}`}>
-            <Toolbar className="f-space-between">
-                <div>
-                    <IconButton
-                        data-testid="botao-menu"
-                        color="inherit"
-                        aria-label="Abrir menu"
-                        edge="start"
-                        onClick={() => menuClick()}
-                        className={classes.menuButton}
-                    >
-                        <Menu/>
-                    </IconButton>
-                </div>
-                <BotaoEntrar data-testid="botao-entrar">
-                    Entrar
-                </BotaoEntrar>
-            </Toolbar>
-        </AppBar>
+        <>
+            <AppBar position="fixed" className={`${classes.appBar}`}>
+                <Toolbar className="f-space-between">
+                    <div>
+                        <IconButton
+                            data-testid="botao-menu"
+                            color="inherit"
+                            aria-label="Abrir menu"
+                            edge="start"
+                            onClick={() => menuClick()}
+                            className={classes.menuButton}
+                        >
+                            <Menu/>
+                        </IconButton>
+                    </div>
+                    <BotaoEntrar data-testid="botao-entrar">
+                        Entrar
+                    </BotaoEntrar>
+                </Toolbar>
+            </AppBar>
+        </>
     );
 }
 
