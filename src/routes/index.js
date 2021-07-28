5bdc463e366e9d63db58ab83292c355a88ece08f
Bruno Alves <bruno-alves2010@live.com>
2021-07-28 17:45:20 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,14 +3,14 @@ import {Route, Switch} from "react-router-dom";
 import ROTAS from "../shared/constants/rotas.const";
 import AutenticacaoProvider from "../shared/context/autenticacao.context";
 
-const Navegacao = lazy(() => import("../pages/MenuLateralContainer/MenuLateralContainer"));
+const MenuLateralContainer = lazy(() => import("../pages/MenuLateralContainer/MenuLateralContainer"));
 
 export default function RoutesContainer() {
     return (
         <Suspense fallback={<></>}>
             <AutenticacaoProvider>
                 <Switch>
-                    <Route path={ROTAS.INICIAL} extact component={Navegacao}/>
+                    <Route path={ROTAS.INICIAL} extact component={MenuLateralContainer}/>
                 </Switch>
             </AutenticacaoProvider>
         </Suspense>
