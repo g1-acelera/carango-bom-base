58283a17f9fca13b72816fbebca50dd764d45304
Bruno Alves <bruno-alves2010@live.com>
2021-07-28 09:14:37 -0300

    Add autenticacao context

---
+++ src/shared/context/autenticacao.context.jsx
@@ -0,0 +1,38 @@
+import {createContext, useContext, useState} from "react";
+import {estaLogado} from "../local-storage/local-storage";
+
+export const AutenticacaoContext = createContext({
+    dadosUsuario: {},
+    adicionaDadosUsuario: dados => {},
+    limpaDadosUsuario: () => {},
+    ehUsuarioLogado: estaLogado(),
+    entra: () => {},
+    sai: () => {},
+});
+
+const AutenticacaoProvider = ({children}) => {
+    const [dadosUsuario, setDadosUsuario] = useState(undefined);
+    const [ehUsuarioLogado, setEhUsuarioLogado] = useState(estaLogado());
+
+    function entra(dadosUsuario) {
+        setEhUsuarioLogado(true);
+        setDadosUsuario(dadosUsuario);
+    }
+
+    function sai() {
+        setEhUsuarioLogado(false);
+        setDadosUsuario(undefined);
+    }
+
+    return (
+        <AutenticacaoContext.Provider
+            value={{dadosUsuario, ehUsuarioLogado, entra, sai}}
+        >
+            {children}
+        </AutenticacaoContext.Provider>
+    );
+}
+
+export default AutenticacaoProvider;
+
+export const useAutenticacaoContext = () => useContext(AutenticacaoContext);
