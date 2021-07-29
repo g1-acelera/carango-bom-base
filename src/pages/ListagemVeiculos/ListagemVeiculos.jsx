4364720a3dcbc50eb1c372e122cb291b86b6fb79
Gustavo Jardim <gustavo.spadotto@gmail.com>
2021-07-29 17:28:45 -0300

    Update ListagemVeiculos.jsx

--- src/pages/ListagemVeiculos/ListagemVeiculos.jsx
+++ src/pages/ListagemVeiculos/ListagemVeiculos.jsx
@@ -1,40 +1,36 @@
-import React, {useState, useEffect, useMemo} from 'react'
-import VeiculoService from '../../services/VeiculoService'
-import Tabela from '../../shared/components/Tabela/Tabela'
+import React, { useState, useEffect, useMemo } from "react"
+import VeiculoService from "../../services/VeiculoService"
+import Tabela from "../../shared/components/Tabela/Tabela"
 
 export default function ListagemVeiculos() {
-    const [veiculos, setVeiculos] = useState([])
-    const colunas = useMemo(() => [
-                {
-                    Header: 'Modelo',
-                    accessor: 'nome',
-                },
-                {
-                    Header: 'Marca',
-                    accessor: 'marca.nome',
-                },
-                {
-                    Header: 'Ano',
-                    accessor: 'ano',
-                },
-                {
-                    Header: 'Valor',
-                    accessor: 'valor',
-                },
-    ],[])
-    
-    useEffect(() => carregarVeiculos(), [])
-    
-    function carregarVeiculos() {
-        VeiculoService.listar()
-        .then((result) => setVeiculos(result))
-    }
-    
-    return (
-        <Tabela 
-        columns={colunas}
-        data={veiculos}
-        titulo="Veículos"
-        />
-    )
+  const [veiculos, setVeiculos] = useState([])
+  const colunas = useMemo(
+    () => [
+      {
+        Header: "Modelo",
+        accessor: "nome",
+      },
+      {
+        Header: "Marca",
+        accessor: "marca.nome",
+      },
+      {
+        Header: "Ano",
+        accessor: "ano",
+      },
+      {
+        Header: "Valor R$",
+        accessor: "valor",
+      },
+    ],
+    []
+  )
+
+  useEffect(() => carregarVeiculos(), [])
+
+  function carregarVeiculos() {
+    VeiculoService.listar().then((result) => setVeiculos(result))
+  }
+
+  return <Tabela columns={colunas} data={veiculos} />
 }
