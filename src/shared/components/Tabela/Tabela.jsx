ec44597bde1db3320d9be54a5b454652332bbbdd
Gustavo Jardim <gustavo.spadotto@gmail.com>
2021-07-29 17:28:59 -0300

    Update Tabela.jsx

--- src/shared/components/Tabela/Tabela.jsx
+++ src/shared/components/Tabela/Tabela.jsx
@@ -1,75 +1,92 @@
-import React from "react";
-import { useTable, useFilters, useGlobalFilter } from "react-table";
+import React from "react"
+import { useTable, useSortBy, useFilters, useGlobalFilter } from "react-table"
 
-import MaUTable from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
+import Table from "@material-ui/core/Table"
+import TableBody from "@material-ui/core/TableBody"
+import TableCell from "@material-ui/core/TableCell"
+import TableHead from "@material-ui/core/TableHead"
+import TableRow from "@material-ui/core/TableRow"
+import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward"
+import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward"
+import TableContainer from "@material-ui/core/TableContainer"
 
-import FiltroGlobal from "./FiltroGlobal";
-import FiltroDeColuna from "./FiltroDeColuna";
-import Typography from "@material-ui/core/Typography";
+import FiltroGlobal from "./FiltroGlobal"
+import FiltroDeColuna from "./FiltroDeColuna"
+import Typography from "@material-ui/core/Typography"
 
-export default function Tabela({ columns, data, titulo }) {
+export default function Tabela({ columns, data }) {
   const defaultColumn = React.useMemo(
     () => ({
       Filter: FiltroDeColuna,
     }),
     []
-  );
+  )
 
   const {
-    getTableProps,
     headerGroups,
     rows,
     prepareRow,
     state,
     preFilteredRows,
     setGlobalFilter,
-  } = useTable({ columns, defaultColumn, data }, useFilters, useGlobalFilter);
+  } = useTable(
+    { columns, defaultColumn, data },
+    useFilters,
+    useGlobalFilter,
+    useSortBy
+  )
 
   return (
-    <MaUTable {...getTableProps()}>
-      <TableHead>
-        <TableRow>
-          <TableCell>
-            <Typography variant="h5">{titulo}</Typography>
-          </TableCell>
-          <TableCell>
+    <TableContainer>
+      <Table>
+        <TableHead>
+          <TableRow>
             <FiltroGlobal
               preFilteredRows={preFilteredRows}
               setGlobalFilter={setGlobalFilter}
               globalFilter={state.globalFilter}
             />
-          </TableCell>
-        </TableRow>
-        {headerGroups.map((headerGroup) => (
-          <TableRow {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <TableCell {...column.getHeaderProps()}>
-                <Typography variant="h6">{column.render("Header")}</Typography>
-              </TableCell>
-            ))}
           </TableRow>
-        ))}
-      </TableHead>
-      <TableBody>
-        {rows.map((row, i) => {
-          prepareRow(row);
-          return (
-            <TableRow {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return (
-                  <TableCell {...cell.getCellProps()}>
-                    {cell.render("Cell")}
-                  </TableCell>
-                );
-              })}
+          {headerGroups.map((headerGroup) => (
+            <TableRow {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <TableCell
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                >
+                  <Typography variant="h6">
+                    {column.render("Header")}
+                    {column.isSorted ? (
+                      column.isSortedDesc ? (
+                        <ArrowDownwardIcon fontSize="small" />
+                      ) : (
+                        <ArrowUpwardIcon fontSize="small" />
+                      )
+                    ) : (
+                      ""
+                    )}
+                  </Typography>
+                </TableCell>
+              ))}
             </TableRow>
-          );
-        })}
-      </TableBody>
-    </MaUTable>
-  );
+          ))}
+        </TableHead>
+        <TableBody>
+          {rows.map((row, i) => {
+            prepareRow(row)
+            return (
+              <TableRow {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <TableCell {...cell.getCellProps()}>
+                      {cell.render("Cell")}
+                    </TableCell>
+                  )
+                })}
+              </TableRow>
+            )
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
 }
