fec9fc481bdfdb3144ec50025b4111c5cf9793a1
Gustavo Jardim <gustavo.spadotto@gmail.com>
2021-07-29 17:28:51 -0300

    Update FiltroDeColuna.jsx

--- src/shared/components/Tabela/FiltroDeColuna.jsx
+++ src/shared/components/Tabela/FiltroDeColuna.jsx
@@ -1,16 +1,15 @@
-
 export default function FiltroDeColuna({
-    column: { filterValue, preFilteredRows, setFilter },
-  }) {
-    const count = preFilteredRows.length
-  
-    return (
-      <input
-        value={filterValue || ''}
-        onChange={e => {
-          setFilter(e.target.value || undefined) // Set undefined to remove the filter entirely
-        }}
-        placeholder={`Search ${count} records...`}
-      />
-    )
-  }
+  column: { filterValue, preFilteredRows, setFilter },
+}) {
+  const count = preFilteredRows.length
+
+  return (
+    <input
+      value={filterValue || ""}
+      onChange={(e) => {
+        setFilter(e.target.value || undefined)
+      }}
+      placeholder={`Search ${count} records...`}
+    />
+  )
+}
