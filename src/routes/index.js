acc3bb3676c63968d99253da1f7635c2b56e2dff
Gustavo Jardim <gustavo.spadotto@gmail.com>
2021-07-29 15:51:02 -0300

    Adicionando página de listagem de veículos ao router

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import {Route, Switch} from "react-router-dom";
 
 const CadastroMarca = lazy(() => import("../pages/cadastro-marca/CadastroMarca"));
 const ListagemMarcas = lazy(() => import("../pages/listagem-marca/ListagemMarcas"));
+const ListagemVeiculos = lazy(() => import("../pages/ListagemVeiculos/ListagemVeiculos"));
 
 export default function RoutesContainer() {
     return (
@@ -10,7 +11,8 @@ export default function RoutesContainer() {
             <Switch>
                 <Route path="/cadastro-marca" component={CadastroMarca}/>
                 <Route path='/alteracao-marca/:id' component={CadastroMarca}/>
-                <Route path="/" component={ListagemMarcas}/>
+                <Route path="/veiculos" component={ListagemVeiculos}/>
+                <Route exact path="/" component={ListagemMarcas}/>
             </Switch>
         </Suspense>
     );
