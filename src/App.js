fc05d9af95b50b102234aadd0a42e7040fb4c304
Gustavo Jardim <gustavo.spadotto@gmail.com>
2021-07-30 11:32:52 -0300

    Removed article component from container

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import React from 'react';
+import React from "react"
 
-import {ThemeProvider} from '@material-ui/core/styles';
-import {muiTheme, defaultStyles} from './@material/Theme'
-import {Container, CssBaseline} from '@material-ui/core';
+import { ThemeProvider } from "@material-ui/core/styles"
+import { muiTheme, defaultStyles } from "./@material/Theme"
+import { CssBaseline } from "@material-ui/core"
 
-import Routes from "./routes";
+import Routes from "./routes"
 
 const App = () => {
-    const classes = defaultStyles();
+  const classes = defaultStyles()
 
-    return (
-        <ThemeProvider theme={muiTheme}>
-            <div className={classes.root}>
-                <CssBaseline/>
-                <main className={classes.content}>
-                    <div className={classes.toolbar}/>
-                    <Container component="article" maxWidth="md">
-                        <Routes/>
-                    </Container>
-                </main>
-            </div>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={muiTheme}>
+      <div className={classes.root}>
+        <CssBaseline />
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <Routes />
+        </main>
+      </div>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
