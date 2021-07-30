ac853acda9e81374034fd09b6089c2ee6426fbda
Bruno Alves <bruno-alves2010@live.com>
2021-07-30 13:11:23 -0300

    Adicionando condições de renderização dos itens

--- src/shared/components/Menu/MenuLateralItem.jsx
+++ src/shared/components/Menu/MenuLateralItem.jsx
@@ -1,27 +1,44 @@
 import React from "react";
-import {Divider, List, ListItem, ListItemIcon, ListItemText} from "@material-ui/core";
-import MENU_ITEM from "./constants/menu-item.const";
-import sidebarStyles from "../../../@material/Sidebar";
+import {Router, useHistory} from "react-router";
 import {NavLink} from "react-router-dom";
 
+import {Divider, List, ListItem, ListItemIcon, ListItemText} from "@material-ui/core";
+import sidebarStyles from "../../../@material/Sidebar";
+
+import MENU_ITEM from "./constants/menu-item.const";
+import {useAutenticacaoContext} from "../../context/autenticacao.context";
+
 const MenuLateralItem = () => {
+    const history = useHistory();
+    const {ehUsuarioLogado} = useAutenticacaoContext();
     const classes = sidebarStyles();
+
+    function itemMenu(item, index) {
+        return (
+            <Router history={history} key={index}>
+                <NavLink to={item.url} activeClassName="ativo">
+                    <ListItem button to={item.url} data-testid={item.testeId}>
+                        <ListItemIcon>{item.icone}</ListItemIcon>
+                        <ListItemText primary={item.nome}/>
+                    </ListItem>
+                </NavLink>
+            </Router>
+        );
+    }
+
+    function renderizaItens(item, index) {
+        if (ehUsuarioLogado) return itemMenu(item, index);
+        return !item.autenticadoParaVisualizar ? itemMenu(item, index) : null;
+    }
 
     return (
         <>
             <div className={classes.toolbar}>
-                <h1 className="principal700-fg titulo">Carango bom</h1>
+                <h1 className="principal700-fg titulo" data-testid="titulo">Carango bom</h1>
             </div>
             <Divider/>
             <List>
-                {MENU_ITEM.map((item, index) => (
-                    <NavLink to={item.url} key={index} activeClassName="ativo">
-                        <ListItem button to={item.url}>
-                            <ListItemIcon>{item.icone}</ListItemIcon>
-                            <ListItemText primary={item.nome}/>
-                        </ListItem>
-                    </NavLink>
-                ))}
+                {MENU_ITEM.map((item, index) => renderizaItens(item, index))}
             </List>
         </>
     );
