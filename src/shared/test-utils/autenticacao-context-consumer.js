85362f86549742361176c23b747403ff89da4206
Bruno Alves <bruno-alves2010@live.com>
2021-08-02 20:02:17 -0300

    Adicionando mock para contexto de autenticação

--- src/shared/test-utils/autenticacao-context-consumer.js
+++ src/shared/test-utils/autenticacao-context-consumer.js
@@ -23,6 +23,19 @@ export const BotaoSimular = ({value}) => {
     );
 }
 
+export const AutenticacaoProviderMock = ({children}) => {
+    const dadosUsuario = {};
+    const ehUsuarioLogado = jest.fn();
+    const adicionaDadosUsuario = jest.fn();
+    const removeDadosUsuario = jest.fn();
+
+    return (
+        <AutenticacaoContext.Provider value={{dadosUsuario, ehUsuarioLogado, adicionaDadosUsuario, removeDadosUsuario}}>
+            {children}
+        </AutenticacaoContext.Provider>
+    );
+}
+
 export const usuarioAutenticacao = {
     tipo: "admin",
     token: "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBUEkgdmVuZGEgZGUgdmVpY3Vsb3MiLCJzdWIiOiIxIiwiaWF0IjoxNjI3OTE1MDQ2LCJleHAiOjE2MjgwMDE0NDZ9.WLLBTcnC9WHAonY5pn2iGG6r-AMa1U_kcTHWAntTJGQ"
