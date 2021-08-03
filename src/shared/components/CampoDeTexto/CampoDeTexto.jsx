501a0404b8f69164fc7ce814b30984827202a02d
Bruno Alves <bruno-alves2010@live.com>
2021-08-03 09:10:27 -0300

    Adicionando tipo do input

--- src/shared/components/CampoDeTexto/CampoDeTexto.jsx
+++ src/shared/components/CampoDeTexto/CampoDeTexto.jsx
@@ -8,13 +8,14 @@ function CampoDeTexto({
   label,
   error,
   onChange,
+  type = "text",
   required = false
 }) {
     return (
         <TextField
             data-testid="text-field"
             id={id}
-            type="text"
+            type={type}
             name={name}
             label={label}
             value={value}
