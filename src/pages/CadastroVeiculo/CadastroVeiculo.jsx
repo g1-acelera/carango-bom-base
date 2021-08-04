b77f80d583f8d7d66f4345a0c464c8f0febecfd6
Bruno Alves <bruno-alves2010@live.com>
2021-08-04 11:28:52 -0300

    Update CadastroVeiculo.jsx

--- src/pages/CadastroVeiculo/CadastroVeiculo.jsx
+++ src/pages/CadastroVeiculo/CadastroVeiculo.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
 
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import VeiculoService from "../../services/VeiculoService";
 import Veiculo from "../../shared/models/Veiculo";
 
@@ -16,80 +16,80 @@ import BotaoDetalhes from "../../shared/components/BotaoDetalhes/BotaoDetalhes";
 import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles((theme) => ({
-  divSpace: {
-    marginTop: '20px',
-  },
-  divPadding: {
-    paddingTop: '20px',
-  },
+    divSpace: {
+        marginTop: '20px',
+    },
+    divPadding: {
+        paddingTop: '20px',
+    },
 }));
 
 
 function CadastroVeiculo() {
-  const classes = useStyles();
-  const {atualizaValor, valores, setValores} = useForm(Veiculo.initialValues());
-  const {dadosConsultados} = useConsultaEntidade(VeiculoService.consultar);
+    const classes = useStyles();
+    const {atualizaValor, valores, setValores} = useForm(Veiculo.initialValues());
+    const {dadosConsultados} = useConsultaEntidade(VeiculoService.consultar);
 
-  useEffect(() => setValores(dadosConsultados), [dadosConsultados, setValores]);
+    useEffect(() => setValores(dadosConsultados), [dadosConsultados, setValores]);
 
 
-  return (
-    <h1>Cadastrar Veículo
-      <Formulario
-        cadastroServico={VeiculoService.cadastrar}
-        alteraServico={VeiculoService.alterar}
-        ehValido={Veiculo.ehVeiculoValido(valores)}
-        valores={valores}>
+    return (
+        <h1>Cadastrar Veículo
+            <Formulario
+                cadastroServico={VeiculoService.cadastrar}
+                alteraServico={VeiculoService.alterar}
+                ehValido={Veiculo.ehVeiculoValido(valores)}
+                valores={valores}>
 
-        <CampoDeTexto
-          id="veiculo-modelo"
-          name="nome"
-          label="Modelo"
-          value={valores
-          ?.nome || ""}
-          required={true}
-          onChange={atualizaValor}/>
+                <CampoDeTexto
+                    id="veiculo-modelo"
+                    name="nome"
+                    label="Modelo"
+                    value={valores
+                        ?.nome || ""}
+                    required={true}
+                    onChange={atualizaValor}/>
 
-        <TextField
-          value={valores?.value}
-          onChange={atualizaValor}
-          name="valor"
-          data-testid="veiculo-valor-text-field"
-          id="formatted-numberformat-input"
-          InputProps={{
-            inputComponent: CampoDeValor,
-          }}
-          variant="outlined"
-          label="valor"
-          required={true}
-          fullWidth
-          className={classes.divSpace}
-        />
+                <TextField
+                    value={valores?.value}
+                    onChange={atualizaValor}
+                    name="valor"
+                    data-testid="veiculo-valor-text-field"
+                    id="formatted-numberformat-input"
+                    InputProps={{
+                        inputComponent: CampoDeValor,
+                    }}
+                    variant="outlined"
+                    label="valor"
+                    required={true}
+                    fullWidth
+                    className={classes.divSpace}
+                />
 
-        <CampoDeTexto
-          id="veiculo-ano"
-          name="ano"
-          label="Ano"
-          value={valores
-          ?.ano || ""}
-          required={true}
-          onChange={atualizaValor}/>
+                <CampoDeTexto
+                    id="veiculo-ano"
+                    name="ano"
+                    label="Ano"
+                    value={valores
+                        ?.ano || ""}
+                    required={true}
+                    onChange={atualizaValor}/>
 
-        <CampoDeSelecao
-          id="veiculo-marca-id"
-          name="marcaId"
-          value={valores
-          ?.marcaId || ""}
-          label="Marca"
-          fullWidth
-          onChange={atualizaValor}
-          className={classes.divSpace}></CampoDeSelecao>
+                <CampoDeSelecao
+                    id="veiculo-marca-id"
+                    name="marcaId"
+                    value={valores
+                        ?.marcaId || ""}
+                    label="Marca"
+                    fullWidth
+                    onChange={atualizaValor}
+                    className={classes.divSpace}/>
 
-        <BotaoDetalhes
-          salvarDesabilidato={!Veiculo.ehVeiculoValido(valores)}/>
-      </Formulario>
-    </h1>
-  );
+                <BotaoDetalhes
+                    salvarDesabilidado={!Veiculo.ehVeiculoValido(valores)}/>
+            </Formulario>
+        </h1>
+    );
 
 }
 
