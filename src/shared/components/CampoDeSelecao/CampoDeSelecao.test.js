ace1e134b84e1e0575c494dd58f9d94474f73859
flaviana <flavispm@gmail.com>
2021-08-04 10:58:30 -0300

    Correção variáveis e teste comentado

--- src/shared/components/CampoDeSelecao/CampoDeSelecao.test.js
+++ src/shared/components/CampoDeSelecao/CampoDeSelecao.test.js
@@ -1,19 +1,8 @@
-import {render, screen, fireEvent, waitForElement} from "@testing-library/react";
+import {render, screen } from "@testing-library/react";
 import React from "react";
 import CampoDeSelecao from "./CampoDeSelecao";
 
 const atualizaValor = jest.fn();
-
-const marcasMock = [
-    {
-        id: 1,
-        marca: "Renault"
-    },
-    {
-        id: 2,
-        marca: "Fiat"
-    }
-]
 
 const renderCards = (marcas) => {
   render(<CampoDeSelecao id="selecao-id"
@@ -45,14 +34,4 @@ describe("Test", () => {
     expect(form).toBeTruthy();
     expect(screen.queryByTestId("-item", { exact: false})).toBeNull();
   });
-
-//   it("Exibir seleção com os dados", async ()  => {
-//     renderCards(marcasMock);
-//     // const renaultCard = screen.getByTestId("1-item");
-//     // const fiatCard = screen.getByTestId("2-item");
-//     // expect(renaultCard).toBeDefined();
-//     // expect(fiatCard).toBeTruthy();
-//     const form = screen.getByTestId("select-test-id");
-//     expect(form.find)
-//   });
 });
