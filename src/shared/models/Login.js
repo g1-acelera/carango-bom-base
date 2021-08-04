bb8726aa977d3c78ccfa23e9fe7eb141d2db6192
Bruno Alves <bruno-alves2010@live.com>
2021-08-04 10:05:20 -0300

    Update Login.js

--- src/shared/models/Login.js
+++ src/shared/models/Login.js
@@ -1,20 +1,9 @@
-import validaEmail from "../validators/email/email";
-
 class LoginModelo {
     static valoresIniciais() {
         return {
             email: "",
             senha: "",
         }
-    }
-
-    static validacoesEmail(valorDoCampo) {
-        return validaEmail(valorDoCampo);
-    }
-
-    static ehModeloValido(valores) {
-        return !!this.validacoesEmail(valores?.email) || !this.valoresIniciais().email ||
-            !this.valoresIniciais().senha;
     }
 }
 
