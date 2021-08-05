1763d8dfc6e556f3fb57a5a3a741f3e8c5e0ef16
Gustavo Jardim <gustavo.spadotto@gmail.com>
2021-08-05 15:35:49 -0300

    Marca e Veiculo Service tests

---
+++ src/services/MarcaService.test.js
@@ -0,0 +1,92 @@
+import MarcaService from "./MarcaService"
+import fetchMock from "jest-fetch-mock"
+
+fetchMock.enableMocks();
+
+beforeEach(() => {
+    fetch.resetMocks();
+});
+
+describe("Testes de MarcaService", () => {
+    it("Deve retornar a marca", async () => {
+        const expected = {id: 1, nome: "Ford"}
+        fetch.mockResponseOnce(JSON.stringify(expected));
+        const response = await MarcaService.consultar(1)
+        expect(response).toStrictEqual(expected)
+    })
+    it("Deve retornar marcas", async () => {
+        const expected = [
+            {id: 1, nome: "Fiat"},
+            {id: 2, nome: "Ford"},
+            {id: 3, nome: "Peugeot"},
+            {id: 4, nome: "Volkswagen"}
+        ]
+        fetch.mockResponseOnce(JSON.stringify(expected));
+        const response = await MarcaService.listar()
+        expect(response).toStrictEqual(expected)
+    })
+    it("Não deve retornar uma marca", async () => {
+        const expected = {status: 404}
+        fetch.mockResponseOnce(JSON.stringify(expected));
+        const response = await MarcaService.consultar(1)
+        expect(response).toStrictEqual(expected)
+    })
+    it("Não deve retornar marcas", async () => {
+        const expected = []
+        fetch.mockResponseOnce(JSON.stringify(expected));
+        const response = await MarcaService.listar()
+        expect(response).toStrictEqual(expected)
+    })
+    it("Deve cadastrar marcar", async () => {
+        const marca = {nome: "Teste"}
+        const expected = {id:1, nome: "Teste"}
+        fetch.mockResponseOnce(JSON.stringify(expected));
+        const response = await MarcaService.cadastrar(expected)
+        expect(response).toStrictEqual(expected)
+    })
+    it("Não deve alterar uma marca", async () => {
+        const expected = {status: 404}
+        fetch.mockResponseOnce(JSON.stringify(expected));
+        const response = await MarcaService.consultar(1)
+        expect(response).toStrictEqual(expected)
+    })
+    it("Deve alterar marcar", async () => {
+        const expected = {id:26, nome: "Volkswagen"}
+        fetch.mockResponseOnce(JSON.stringify(expected));
+        const response = await MarcaService.alterar(expected)
+        expect(response).toStrictEqual(expected)
+    })
+    it("Deve excluir marcar", async () => {
+        fetch.mockResponseOnce(JSON.stringify(null));
+        const response = await MarcaService.excluir(1)
+        expect(response).toStrictEqual(null)
+    })
+    it("Não deve excluir uma marca", async () => {
+        const expected = {status: 404}
+        fetch.mockResponseOnce(JSON.stringify(expected));
+        const response = await MarcaService.consultar(1)
+        expect(response).toStrictEqual(expected)
+    })
+})
+describe("Testes de MarcaService com Token incorreto ou inexistente", () => {
+    it("Deve retornar erro 403 ao cadastrar", async () => {
+        const marca = {nome: "Teste"}
+        const expected = {status: 403}
+        fetch.mockResponseOnce(JSON.stringify(expected));
+        const response = await MarcaService.cadastrar(marca)
+        expect(response).toStrictEqual(expected)
+    })
+    it("Deve retornar erro 403 ao excluir", async () => {
+        const expected = {status: 403}
+        fetch.mockResponseOnce(JSON.stringify(expected));
+        const response = await MarcaService.excluir(1)
+        expect(response).toStrictEqual(expected)
+    })
+    it("Deve retornar erro 403 ao alterar", async () => {
+        const marca = {id:26, nome: "Volkswagen"}
+        const expected = {status: 403}
+        fetch.mockResponseOnce(JSON.stringify(expected));
+        const response = await MarcaService.alterar(marca)
+        expect(response).toStrictEqual(expected)
+    })
+})
---
+++ src/services/VeiculoService.test.js
@@ -0,0 +1,143 @@
+import VeiculoService from "./VeiculoService"
+import fetchMock from "jest-fetch-mock"
+
+fetchMock.enableMocks();
+
+beforeEach(() => {
+    fetch.resetMocks();
+});
+
+describe("Testes de VeiculoService", () => {
+    it("Deve retornar a veiculo", async () => {
+        const expected = {
+            id: 1, 
+            ano: 2019, 
+            nome: "Focus", 
+            valor: 19900.99, 
+            veiculo: {id:1, nome: "Ford"}
+        }
+        fetch.mockResponseOnce(JSON.stringify(expected));
+        const response = await VeiculoService.consultar(1)
+        expect(response).toStrictEqual(expected)
+    })
+    it("Deve retornar veiculos", async () => {
+        const expected = [
+            {
+                id: 1, 
+                ano: 2019, 
+                nome: "Golf", 
+                valor: 89900, 
+                veiculo: {id:1, nome: "Volkswagen"}
+            },
+            {
+                id: 1, 
+                ano: 2019, 
+                nome: "Focus", 
+                valor: 19900.99, 
+                veiculo: {id:2, nome: "Ford"}
+            },
+            {
+                id: 1, 
+                ano: 2008, 
+                nome: "Uno de firma com Escada", 
+                valor: 15000, 
+                veiculo: {id:3, nome: "Fiat"}
+            },
+        ]
+        fetch.mockResponseOnce(JSON.stringify(expected));
+        const response = await VeiculoService.listar()
+        expect(response).toStrictEqual(expected)
+    })
+    it("Não deve retornar um veiculo", async () => {
+        const expected = {status: 404}
+        fetch.mockResponseOnce(JSON.stringify(expected));
+        const response = await VeiculoService.consultar(1)
+        expect(response).toStrictEqual(expected)
+    })
+    it("Não deve retornar veiculos", async () => {
+        const expected = []
+        fetch.mockResponseOnce(JSON.stringify(expected));
+        const response = await VeiculoService.listar()
+        expect(response).toStrictEqual(expected)
+    })
+    it("Deve cadastrar veiculor", async () => {
+        const expected = {
+            id: 1, 
+            ano: 2008, 
+            nome: "Uno de firma com Escada", 
+            valor: 15000, 
+            veiculo: {id:3, nome: "Fiat"}
+        }
+        const veiculo = {
+            ano: 2008, 
+            nome: "Uno de firma com Escada", 
+            valor: 15000, 
+            veiculo: {id:3, nome: "Fiat"}
+        }
+        fetch.mockResponseOnce(JSON.stringify(expected));
+        const response = await VeiculoService.cadastrar(veiculo)
+        expect(response).toStrictEqual(expected)
+    })
+    it("Deve alterar veiculor", async () => {
+        const expected = {
+            id: 39, 
+            ano: 2019, 
+            nome: "Golf", 
+            valor: 89900, 
+            veiculo: {id:1, nome: "Volkswagen"}
+        }
+        fetch.mockResponseOnce(JSON.stringify(expected));
+        const response = await VeiculoService.alterar(expected)
+        expect(response).toStrictEqual(expected)
+    })
+    it("Não deve alterar um veiculo", async () => {
+        const expected = {status: 404}
+        fetch.mockResponseOnce(JSON.stringify(expected));
+        const response = await VeiculoService.consultar(1)
+        expect(response).toStrictEqual(expected)
+    })
+    it("Deve excluir veiculor", async () => {
+        fetch.mockResponseOnce(JSON.stringify(null));
+        const response = await VeiculoService.excluir(1)
+        expect(response).toStrictEqual(null)
+    })
+    it("Não deve excluir um veiculo", async () => {
+        const expected = {status: 404}
+        fetch.mockResponseOnce(JSON.stringify(expected));
+        const response = await VeiculoService.consultar(1)
+        expect(response).toStrictEqual(expected)
+    })
+});
+describe("Testes de VeiculoService com Token incorreto ou inexistente", () => {
+    it("Deve retornar erro 403 ao cadastrar", async () => {
+        const veiculo = {
+            ano: 2019, 
+            nome: "Golf", 
+            valor: 89900, 
+            veiculo: {id:1, nome: "Volkswagen"}
+        }
+        const expected = {status: 403}
+        fetch.mockResponseOnce(JSON.stringify(expected));
+        const response = await VeiculoService.cadastrar(veiculo)
+        expect(response).toStrictEqual(expected)
+    })
+    it("Deve retornar erro 403 ao excluir", async () => {
+        const expected = {status: 403}
+        fetch.mockResponseOnce(JSON.stringify(expected));
+        const response = await VeiculoService.excluir(19)
+        expect(response).toStrictEqual(expected)
+    })
+    it("Deve retornar erro 403 ao alterar", async () => {
+        const veiculo = {
+            id: 28, 
+            ano: 2008, 
+            nome: "Uno de firma com Escada", 
+            valor: 15000, 
+            veiculo: {id:3, nome: "Fiat"}
+        }
+        const expected = {status: 403}
+        fetch.mockResponseOnce(JSON.stringify(expected));
+        const response = await VeiculoService.alterar(veiculo)
+        expect(response).toStrictEqual(expected)
+    })
+})
