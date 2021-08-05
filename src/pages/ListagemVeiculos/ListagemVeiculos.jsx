49967e8beb46e3ea657482f808ba0a1739b8c6d6
Gustavo Jardim <gustavo.spadotto@gmail.com>
2021-08-05 10:22:52 -0300

    Update ListagemVeiculos.jsx

--- src/pages/ListagemVeiculos/ListagemVeiculos.jsx
+++ src/pages/ListagemVeiculos/ListagemVeiculos.jsx
@@ -1,4 +1,11 @@
 import React, { useState, useEffect, useMemo } from "react"
+import {useHistory} from 'react-router-dom';
+
+import AddIcon from '@material-ui/icons/Add';
+import {fabStyles} from '../../@material/Button';
+import {Fab} from '@material-ui/core';
+
+import ROTAS from "../../shared/constants/rotas.const"
 import {useAutenticacaoContext} from "../../shared/context/autenticacao.context";
 import VeiculoService from "../../services/VeiculoService"
 import Tabela from "../../shared/components/Tabela/Tabela"
@@ -6,7 +13,8 @@ import Tabela from "../../shared/components/Tabela/Tabela"
 export default function ListagemVeiculos() {
   const [veiculos, setVeiculos] = useState([])
   const {ehUsuarioLogado} = useAutenticacaoContext();
-  const deveTerColunaDeAcoes = ehUsuarioLogado? true : false;
+  const history = useHistory();
+  const classes = fabStyles();
 
   const colunas = useMemo(
     () => [
@@ -38,12 +46,18 @@ export default function ListagemVeiculos() {
   }
 
   return (
-    <Tabela 
-      columns={colunas} 
-      data={veiculos}
-      colunaDeAcoes={deveTerColunaDeAcoes}
-      service={VeiculoService}
-      caminhoDoObjeto="/veiculos"
-    />
+    <div>
+      <Tabela 
+        columns={colunas} 
+        data={veiculos}
+        colunaDeAcoes={ehUsuarioLogado? true : false}
+        service={VeiculoService}
+        caminhoDoObjeto="/veiculos"
+      />
+      <Fab 
+        color="primary" aria-label="add" className={classes.fab} onClick={() => history.push(ROTAS.CADASTRO_VEICULO)}>
+        <AddIcon/>
+      </Fab>
+    </div>
   )
 }
