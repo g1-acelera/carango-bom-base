94f23f5a9595340c7fec26e701841301b4889ebe
Bruno Alves <bruno-alves2010@live.com>
2021-08-05 14:06:36 -0300

    Adição das rotas de cadastro e edição de usuários

--- src/pages/MenuLateralContainer/MenuLateralContainer.jsx
+++ src/pages/MenuLateralContainer/MenuLateralContainer.jsx
@@ -10,6 +10,8 @@ const ListagemMarcas = lazy(() => import("../ListagemMarca/ListagemMarcas"));
 const ListagemVeiculos = lazy(() => import("../ListagemVeiculos/ListagemVeiculos"));
 const Dashboard = lazy(() => import("../Dashboard/Dashboard"));
 const PaginaNaoEncontrada = lazy(() => import("../PaginaNaoEncontrada/PaginaNaoEncontrada"));
+const ListagemUsuarios = lazy(() => import("../ListagemUsuarios/ListagemUsuarios"));
+const CadastroUsuario = lazy(() => import("../CadastroUsuario/CadastroUsuario"));
 
 const MenuLateralContainer = () => {
     return (
@@ -22,6 +24,9 @@ const MenuLateralContainer = () => {
                 <RotaProtegida path={ROTAS.MARCAS} component={ListagemMarcas}/>
                 <RotaProtegida path={ROTAS.CADASTRO_VEICULO} component={CadastroVeiculo}/>
                 <RotaProtegida path={ROTAS.ALTERACAO_VEICULO} component={CadastroVeiculo}/>
+                <RotaProtegida path={ROTAS.USUARIOS} component={ListagemUsuarios}/>
+                <RotaProtegida path={ROTAS.CADASTRO_USUARIO} component={CadastroUsuario}/>
+                <RotaProtegida path={ROTAS.ALTERACAO_USUARIO} component={CadastroUsuario}/>
                 <Route path={ROTAS.VEICULOS} component={ListagemVeiculos}/>
                 <Route path={ROTAS.NAO_ENCONTRADO} exact component={PaginaNaoEncontrada}/>
             </Switch>
--- src/pages/MenuLateralContainer/MenuLateralContainer.test.js
+++ src/pages/MenuLateralContainer/MenuLateralContainer.test.js
@@ -5,6 +5,9 @@ import Dashboard from "../Dashboard/Dashboard";
 import CadastroMarca from "../CadastroMarca/CadastroMarca";
 import ListagemMarcas from "../ListagemMarca/ListagemMarcas";
 import ListagemVeiculos from "../ListagemVeiculos/ListagemVeiculos";
+import CadastroVeiculo from "../CadastroVeiculo/CadastroVeiculo";
+import ListagemUsuarios from "../ListagemUsuarios/ListagemUsuarios";
+import CadastroUsuario from "../CadastroUsuario/CadastroUsuario";
 import ROTAS from "../../shared/constants/rotas.const";
 import MenuLateralContainer from "./MenuLateralContainer";
 import {AutenticacaoProviderMock} from "../../shared/test-utils/autenticacao-context-consumer";
@@ -15,6 +18,9 @@ jest.mock("../CadastroMarca/CadastroMarca");
 jest.mock("../ListagemMarca/ListagemMarcas");
 jest.mock("../ListagemVeiculos/ListagemVeiculos");
 jest.mock("../PaginaNaoEncontrada/PaginaNaoEncontrada");
+jest.mock("../CadastroVeiculo/CadastroVeiculo");
+jest.mock("../ListagemUsuarios/ListagemUsuarios");
+jest.mock("../CadastroUsuario/CadastroUsuario");
 
 const ComponenteContainer = (rotaInicial) => {
     return render(
@@ -29,45 +35,90 @@ const ComponenteContainer = (rotaInicial) => {
 };
 
 describe("Menu lateral container testes", () => {
-    it("Deve iniciar na página definida como padrão", async () => {
-        Dashboard.mockImplementation(() => <div>Dashboard mock</div>);
-        const {getByText} = ComponenteContainer(ROTAS.INICIAL);
-        const titulo = await waitFor(() => getByText(/Dashboard mock/i));
-        expect(titulo).toBeInTheDocument();
+    describe("Dashboard", () => {
+        it("Deve iniciar na página definida como padrão", async () => {
+            Dashboard.mockImplementation(() => <div>Dashboard mock</div>);
+            const {getByText} = ComponenteContainer(ROTAS.INICIAL);
+            const titulo = await waitFor(() => getByText(/Dashboard mock/i));
+            expect(titulo).toBeInTheDocument();
+        });
     });
 
-    it("Deve navegar para cadastro de marca", async () => {
-        CadastroMarca.mockImplementation(() => <div>Cadastro de marca mock</div>);
-        const {getByText} = ComponenteContainer(ROTAS.CADASTRO_MARCA);
-        const titulo = await waitFor(() => getByText(/Cadastro de marca mock/i));
-        expect(titulo).toBeInTheDocument();
+    describe("Marcas", () => {
+        it("Deve navegar para cadastro de marca", async () => {
+            CadastroMarca.mockImplementation(() => <div>Cadastro de marca mock</div>);
+            const {getByText} = ComponenteContainer(ROTAS.CADASTRO_MARCA);
+            const titulo = await waitFor(() => getByText(/Cadastro de marca mock/i));
+            expect(titulo).toBeInTheDocument();
+        });
+
+        it("Deve navegar para alteração de marca", async () => {
+            CadastroMarca.mockImplementation(() => <div>Alteração de marca mock</div>);
+            const {getByText} = ComponenteContainer(ROTAS.ALTERACAO_MARCA);
+            const titulo = await waitFor(() => getByText(/Alteração de marca mock/i));
+            expect(titulo).toBeInTheDocument();
+        });
+
+        it("Deve navegar para listagem de marcas", async () => {
+            ListagemMarcas.mockImplementation(() => <div>Listagem de marcas mock</div>);
+            const {getByText} = ComponenteContainer(ROTAS.MARCAS);
+            const titulo = await waitFor(() => getByText(/Listagem de marcas mock/i));
+            expect(titulo).toBeInTheDocument();
+        });
     });
 
-    it("Deve navegar para alteração de marca", async () => {
-        CadastroMarca.mockImplementation(() => <div>Alteração de marca mock</div>);
-        const {getByText} = ComponenteContainer(ROTAS.ALTERACAO_MARCA);
-        const titulo = await waitFor(() => getByText(/Alteração de marca mock/i));
-        expect(titulo).toBeInTheDocument();
+    describe("Veículos", () => {
+        it("Deve navegar para listagem de veículos", async () => {
+            ListagemVeiculos.mockImplementation(() => <div>Listagem de veículos mock</div>);
+            const {getByText} = ComponenteContainer(ROTAS.VEICULOS);
+            const titulo = await waitFor(() => getByText(/Listagem de veículos mock/i));
+            expect(titulo).toBeInTheDocument();
+        });
+
+        it("Deve navegar para alteração de veículo", async () => {
+            CadastroVeiculo.mockImplementation(() => <div>Alteração de veículo mock</div>);
+            const {getByText} = ComponenteContainer(ROTAS.ALTERACAO_VEICULO);
+            const titulo = await waitFor(() => getByText(/Alteração de veículo mock/i));
+            expect(titulo).toBeInTheDocument();
+        });
+
+        it("Deve navegar para cadastro de veículo", async () => {
+            CadastroVeiculo.mockImplementation(() => <div>Cadastro de veículo mock</div>);
+            const {getByText} = ComponenteContainer(ROTAS.CADASTRO_VEICULO);
+            const titulo = await waitFor(() => getByText(/Cadastro de veículo mock/i));
+            expect(titulo).toBeInTheDocument();
+        });
     });
 
-    it("Deve navegar para listagem de marcas", async () => {
-        ListagemMarcas.mockImplementation(() => <div>Listagem de marcas mock</div>);
-        const {getByText} = ComponenteContainer(ROTAS.MARCAS);
-        const titulo = await waitFor(() => getByText(/Listagem de marcas mock/i));
-        expect(titulo).toBeInTheDocument();
+    describe("Usuários", () => {
+        it("Deve navegar para listagem de usuários", async () => {
+            ListagemUsuarios.mockImplementation(() => <div>Listagem de usuários mock</div>);
+            const {getByText} = ComponenteContainer(ROTAS.USUARIOS);
+            const titulo = await waitFor(() => getByText(/Listagem de usuários mock/i));
+            expect(titulo).toBeInTheDocument();
+        });
+
+        it("Deve navegar para alteração de usuário", async () => {
+            CadastroUsuario.mockImplementation(() => <div>Alteração de usuário mock</div>);
+            const {getByText} = ComponenteContainer(ROTAS.ALTERACAO_USUARIO);
+            const titulo = await waitFor(() => getByText(/Alteração de usuário mock/i));
+            expect(titulo).toBeInTheDocument();
+        });
+
+        it("Deve navegar para cadastro de usuário", async () => {
+            CadastroUsuario.mockImplementation(() => <div>Cadastro de usuário mock</div>);
+            const {getByText} = ComponenteContainer(ROTAS.CADASTRO_USUARIO);
+            const titulo = await waitFor(() => getByText(/Cadastro de usuário mock/i));
+            expect(titulo).toBeInTheDocument();
+        });
     });
 
-    it("Deve navegar para listagem de veículos", async () => {
-        ListagemVeiculos.mockImplementation(() => <div>Listagem de veículos mock</div>);
-        const {getByText} = ComponenteContainer(ROTAS.VEICULOS);
-        const titulo = await waitFor(() => getByText(/Listagem de veículos mock/i));
-        expect(titulo).toBeInTheDocument();
-    });
-
-    it("Deve navegar para página não encontrada", async () => {
-        PaginaNaoEncontrada.mockImplementation(() => <div>Página não encontrada mock</div>);
-        const {getByText} = ComponenteContainer("/essa-rota-nao-existe");
-        const titulo = await waitFor(() => getByText(/Página não encontrada mock/i));
-        expect(titulo).toBeInTheDocument();
+    describe("Página não encontrada", () => {
+        it("Deve navegar para página não encontrada", async () => {
+            PaginaNaoEncontrada.mockImplementation(() => <div>Página não encontrada mock</div>);
+            const {getByText} = ComponenteContainer("/essa-rota-nao-existe");
+            const titulo = await waitFor(() => getByText(/Página não encontrada mock/i));
+            expect(titulo).toBeInTheDocument();
+        });
     });
 });
