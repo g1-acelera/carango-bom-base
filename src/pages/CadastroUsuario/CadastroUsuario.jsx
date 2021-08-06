b696d997378a15ecfc6c6f88a617418735450cff
Bruno Alves <bruno-alves2010@live.com>
2021-08-05 22:52:36 -0300

    Componente de cadastro de usuário

--- src/pages/CadastroUsuario/CadastroUsuario.jsx
+++ src/pages/CadastroUsuario/CadastroUsuario.jsx
@@ -1,8 +1,44 @@
 import React from "react";
+import Formulario from "../../shared/components/Formulario/Formulario";
+import CampoDeTexto from "../../shared/components/CampoDeTexto/CampoDeTexto";
+import BotaoDetalhes from "../../shared/components/BotaoDetalhes/BotaoDetalhes";
+import useForm from "../../shared/hooks/useForm";
+import Usuario from "../../shared/models/Usuario";
+import UsuarioService from "../../services/UsuarioService";
 
 const CadastroUsuario = () => {
+    const {atualizaValor, valores} = useForm(Usuario.initialValues());
+
     return (
-        <></>
+        <div id="cadastro-veiculo-screen">
+            <h1>
+                Cadastro Usuário
+            </h1>
+
+            <Formulario
+                cadastroServico={UsuarioService.cadastrar}
+                ehValido={Usuario.ehVeiculoValido(valores)}
+                valores={valores}>
+
+                <CampoDeTexto
+                    name="email"
+                    label="Email"
+                    value={valores?.email || ""}
+                    error={Usuario.validacoesEmail(valores?.email)}
+                    required={true}
+                    onChange={atualizaValor}/>
+
+                <CampoDeTexto
+                    name="senha"
+                    label="Senha"
+                    type="password"
+                    value={valores?.senha || ""}
+                    required={true}
+                    onChange={atualizaValor}/>
+
+                <BotaoDetalhes salvarDesabilitado={!Usuario.ehVeiculoValido(valores)}/>
+            </Formulario>
+        </div>
     );
 }
 
