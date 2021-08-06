aef19c71e0075aae7518534b6971b279ae6352a9
Bruno Alves <bruno-alves2010@live.com>
2021-08-06 14:28:14 -0300

    Update Marca.test.js

--- src/shared/models/Marca.test.js
+++ src/shared/models/Marca.test.js
@@ -1,5 +1,9 @@
 import Marca from "./Marca";
 
+const validacoesNome = (nome) => {
+    return Marca.validacoesNome("")
+}
+
 describe("Marca modelo testes", () => {
     it("Deve possuir os valores iniciais", () => {
         const esperado = {
@@ -9,22 +13,22 @@ describe("Marca modelo testes", () => {
     });
 
     it("Deve retornar a validação de obrigatoriedade para o nome", () => {
-        const validacao = Marca.validacoesNome("");
+        const validacao = validacoesNome("");
         expect(validacao).toBe("Campo obrigatório");
     });
 
     it("Não deve retornar a validação de obrigatoriedade quando o nome for preenchido", () => {
-        const validacao = Marca.validacoesNome("Fiat");
+        const validacao = validacoesNome("Fiat");
         expect(validacao).toBe("");
     });
 
     it("Deve retornar a validação de tamanho mínimo para o nome", () => {
-        const validacao = Marca.validacoesNome("aa");
+        const validacao = validacoesNome("aa");
         expect(validacao).toBe("Deve ter ao menos 3 letras");
     });
 
     it("Não deve retornar a validação de tamanho mínimo quando o nome for preenchido", () => {
-        const validacao = Marca.validacoesNome("Fiat");
+        const validacao = validacoesNome("Fiat");
         expect(validacao).toBe("");
     });
 
