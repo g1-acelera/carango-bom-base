e7e8618fae9b60f2e274ff555dc74263034587eb
Bruno Alves <bruno-alves2010@live.com>
2021-08-06 09:19:04 -0300

    Movendo estilos do filtro global

--- src/shared/components/Tabela/ColunaDeAcoes.jsx
+++ src/shared/components/Tabela/ColunaDeAcoes.jsx
@@ -7,9 +7,8 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
-import {IconButton, Button} from "@material-ui/core"
+import {Button, IconButton} from "@material-ui/core"
 import {DeleteOutlineOutlined, EditOutlined} from '@material-ui/icons'
-
 
 export default function ColunaDeAcoes({object_id, service, caminhoDoObjeto}) {
     const history = useHistory()
@@ -23,7 +22,7 @@ export default function ColunaDeAcoes({object_id, service, caminhoDoObjeto}) {
     const excluir = (id) => {
         service.excluir(id)
             .then((response) => {
-                if(response.ok === false) {
+                if (response.ok === false) {
                     fecharDeleteDialog();
                     abrirErroDialog();
                     return;
@@ -34,80 +33,80 @@ export default function ColunaDeAcoes({object_id, service, caminhoDoObjeto}) {
 
     const abrirDeleteDialog = (abrirDialog) => {
         setOpenDeleteDialog(true);
-      };
-    
+    };
+
     const fecharDeleteDialog = () => {
         setOpenDeleteDialog(false);
     };
 
     const abrirErroDialog = () => {
         setOpenErroDialog(true);
-      };
-    
+    };
+
     const fecharErroDialog = () => {
         setOpenErroDialog(false);
     };
 
     return (
         <>
-        <TableCell>
-            <IconButton
-                data-testid="botao-alterar"
-                variant="contained"
-                color="primary"
-                onClick={() => alterar(object_id)}>
-                <EditOutlined color="secondary"/>
-            </IconButton>
-            <IconButton
-                id="1-botao-excluir"
-                data-testid="botao-excluir"
-                variant="contained"
-                color="secondary"
-                onClick={() => abrirDeleteDialog()}>
-                <DeleteOutlineOutlined color="error"/>
-            </IconButton>
-        </TableCell>
-        <Dialog
-            open={openDeleteDialog}
-            onClose={fecharDeleteDialog}
-            aria-labelledby="draggable-dialog-title"
-        >   
-        <DialogTitle style={{ cursor: 'move' }} id="draggable-dialog-title">
-            Cuidado
-        </DialogTitle>
-        <DialogContent>
-            <DialogContentText>
-                Tem certeza que quer excluir este registro?
-            </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-        <Button autoFocus onClick={fecharDeleteDialog} color="primary">
-            Cancelar
-        </Button>
-        <Button onClick={() => excluir(object_id)} color="primary">
-            Excluir
-        </Button>
-        </DialogActions>
-        </Dialog>
-        <Dialog
-            open={openErroDialog}
-            onClose={fecharErroDialog}
-            aria-labelledby="draggable-dialog-title"
-        >   
-        <DialogTitle style={{ cursor: 'move' }} id="draggable-dialog-title">
-            Erro
-        </DialogTitle>
-        <DialogContent>
-            <DialogContentText>
-                Não é possível deletar esta Marca
-            </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-        <Button autoFocus onClick={fecharErroDialog} color="primary">
-            Ok
-        </Button>
-        </DialogActions>
-        </Dialog>
+            <TableCell>
+                <IconButton
+                    data-testid="botao-alterar"
+                    variant="contained"
+                    color="primary"
+                    onClick={() => alterar(object_id)}>
+                    <EditOutlined color="secondary"/>
+                </IconButton>
+                <IconButton
+                    id="1-botao-excluir"
+                    data-testid="botao-excluir"
+                    variant="contained"
+                    color="secondary"
+                    onClick={() => abrirDeleteDialog()}>
+                    <DeleteOutlineOutlined color="error"/>
+                </IconButton>
+            </TableCell>
+            <Dialog
+                open={openDeleteDialog}
+                onClose={fecharDeleteDialog}
+                aria-labelledby="draggable-dialog-title"
+            >
+                <DialogTitle style={{cursor: 'move'}} id="draggable-dialog-title">
+                    Cuidado
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Tem certeza que quer excluir este registro?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button autoFocus onClick={fecharDeleteDialog} color="primary">
+                        Cancelar
+                    </Button>
+                    <Button onClick={() => excluir(object_id)} color="primary">
+                        Excluir
+                    </Button>
+                </DialogActions>
+            </Dialog>
+            <Dialog
+                open={openErroDialog}
+                onClose={fecharErroDialog}
+                aria-labelledby="draggable-dialog-title"
+            >
+                <DialogTitle style={{cursor: 'move'}} id="draggable-dialog-title">
+                    Erro
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Não é possível deletar esta Marca
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button autoFocus onClick={fecharErroDialog} color="primary">
+                        Ok
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </>
     )
 }   
