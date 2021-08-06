5f9e2114155669728bb10be57179bdc7f134fa47
Bruno Alves <bruno-alves2010@live.com>
2021-08-05 23:17:39 -0300

    Adicionando estilos para botões

--- src/@material/Button.js
+++ src/@material/Button.js
@@ -1,13 +1,17 @@
 import Button from '@material-ui/core/Button';
 import {makeStyles} from '@material-ui/core';
 import {withStyles} from '@material-ui/core/styles';
-import {pink} from "@material-ui/core/colors";
+import {blue} from "@material-ui/core/colors";
 
 export const fabStyles = makeStyles(() => ({
     fab: {
         position: 'absolute',
         bottom: '100px',
         right: '100px',
+        boxShadow: "0px 4px 14px 0px rgb(173, 20, 87, .4)",
+        '&:hover': {
+            boxShadow: 'none',
+        },
     },
     actionsToolbar: {
         float: 'right'
@@ -20,12 +24,12 @@ export const fabStyles = makeStyles(() => ({
 
 export const BotaoPrincipal = withStyles({
     root: {
-        backgroundColor: pink[400],
+        backgroundColor: blue[600],
         borderRadius: '20px',
         color: 'white',
-        boxShadow: '0px 4px 4px rgba(236, 64, 122, 0.4)',
+        boxShadow: '0px 4px 14px 0px rgba(30, 136, 229, .4)',
         '&:hover': {
-            backgroundColor: 'rgba(236, 64, 122, .9)',
+            backgroundColor: 'rgba(30, 136, 229, .9)',
             borderColor: 'none',
             boxShadow: 'none',
         },
